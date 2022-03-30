e37c0dd39dcaa89b8d575fa2c1179300a13e62bb
Levi G <bobrobot@zoho.com>
2022-03-30 01:17:47 -0700

    Added error dialog in case of error

--- flutter/lib/feed.dart
+++ flutter/lib/feed.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 
 class FeedPage extends StatefulWidget {
   FeedPage({
@@ -20,21 +21,27 @@ class _HomePageState extends State<FeedPage> {
   var offset = 0;
   var requested = false;
 
-  get feedURL => String;
-
   Future loadFeed() async {
     print("feedload called");
     // Await the http get response, then decode the json-formatted response.
     if (requested == false) {
+      print("getting feed");
       var response = await http.get(Uri.parse(
           "https://api.compensationvr.tk/api/social/imgfeed?offset=${offset}&count=5&reverse"));
       if (response.statusCode == 200) {
         feedJson = json.decode(response.body);
         print("Images Fetched Successfully");
         print(feedJson);
-        setState(() {requested = true;});
+        setState(() {
+          requested = true;
+        });
       } else {
-        print('IMAGE Request failed with status: ${response.statusCode}.');
+        print('Image feed request failed with status: ${response.statusCode}.');
+        showOkAlertDialog(
+          context: context,
+          title: "Error",
+          message: "Image feed request failed with status: ${response.statusCode}."
+        );
       }
     } else {
       print("Already requested");
@@ -72,7 +79,7 @@ class _HomePageState extends State<FeedPage> {
                     Padding(
                       padding: EdgeInsets.all(16.0),
                       child: Text(
-                          "${i.toString()} - ${feedJson[i]["takenBy"]["username"]}: ${feedJson[i]["takenOn"]["humanReadable"]}",
+                          "${feedJson[i]["takenBy"]["username"]}: ${feedJson[i]["takenOn"]["humanReadable"]}",
                           style: const TextStyle(fontWeight: FontWeight.bold),
                           textAlign: TextAlign.left),
                     )
