c60ca1743a43f5c964d5f0fd67161ab7818636a6
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-09-22 16:47:06 -0700

    a minor change
    just a lil

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'feed.dart';
 import 'credits.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(new MyApp());
@@ -11,14 +12,18 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Loading...',
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      theme: MyThemes.lightTheme,
-      darkTheme: MyThemes.darkTheme,
-      home: new HomePage(),
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+          return MaterialApp(
+            title: 'Loading...',
+            themeMode: themeProvider.themeMode,
+            debugShowCheckedModeBanner: false,
+            theme: MyThemes.lightTheme,
+            darkTheme: MyThemes.darkTheme,
+            home: new HomePage(),
+          );
+        },
+      );
 }
--- flutter/lib/provider/theme_provider.dart
+++ flutter/lib/provider/theme_provider.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.dark;
+
+  bool get isDarkMode => themeMode == ThemeMode.dark;
+
+  void toggleTheme(bool isOn) {
+    themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
+
 class MyThemes {
   static final darkTheme = ThemeData(
       colorScheme: ColorScheme.fromSwatch(
