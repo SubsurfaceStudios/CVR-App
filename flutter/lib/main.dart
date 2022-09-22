54c869393118e8e4b597af21874a7215a5ee60bf
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-09-22 15:39:23 -0700

    don't worry about it
    I wouldn't worry about it

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,13 +1,12 @@
-
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'feed.dart';
 import 'credits.dart';
- 
+
 void main() {
   runApp(new MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
