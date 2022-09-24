2067f0e83790be25c80f93ab6bc1ee51eeee3a2b
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-09-24 11:23:01 -0700

    few file changes for consistancy
    Trying to not die later lol

---
+++ flutter/lib/dash.dart
@@ -0,0 +1,10 @@
+//new feed testing area
+//created by: alizardguy (9/24/2022 11:22AM)
+//Project: https://github.com/alizardguy/CompensationVR-App-Tegu
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
