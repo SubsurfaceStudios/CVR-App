eb6e18f57163bc44af8f666c2100d9bb491fa5d4
alizardguy <42082829+alizardguy@users.noreply.github.com>
2022-09-24 11:35:58 -0700

    I forgor

--- flutter/lib/home_page.dart
+++ flutter/lib/home_page.dart
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
               image: AssetImage('assets/cvrpurple.png'),
               width: 200,
               height: 200),
-          Text("CVRNet App TEGU BUILD v1.2.1 (fork of v1.0.2)"),
+          Text("CVRNet App TEGU BUILD v1.3.0 (fork of v1.0.2)"),
           TextButton(
             style: ButtonStyle(
               foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
@@ -56,7 +56,7 @@ class _HomePageState extends State<HomePage> {
                 MaterialPageRoute(builder: (context) => DashPage()),
               );
             },
-            child: Text('Gecko'),
+            child: Text('Gecko (need feed test)'),
           ),
           TextButton(
             style: ButtonStyle(
